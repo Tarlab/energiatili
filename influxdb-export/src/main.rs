6d679e57f221d29f5bc9c29c393159e57ff63d62
Ossi Herrala <oherrala@iki.fi>
2019-10-24 17:26:31 +0300

    Switch from influent to influxdb which seems to be in devel

--- influxdb-export/src/main.rs
+++ influxdb-export/src/main.rs
@@ -1,7 +1,8 @@
 use std::io;
 
-use influent::client::{Client, Credentials, Precision};
-use influent::measurement::{Measurement, Value};
+use influxdb;
+
+use tokio::runtime::current_thread::Runtime;
 
 use energiatili_model::measurement::{Measurements, Resolution, Tariff};
 use energiatili_model::model::Model;
@@ -13,63 +14,48 @@ fn main() {
     let model = Model::from_reader(stdin).expect("read JSON Model");
     let measurements = Measurements::from(&model);
 
-    let credentials = Credentials {
-        username: "",
-        password: "",
-        database: "energiatili",
-    };
+    let client = influxdb::Client::new("http://127.0.0.1:8086", "energiatili");
 
-    let influxdb = influent::create_client(credentials, vec!["http://127.0.0.1:8086"]);
-
-    const SIZE: usize = 1000;
-    let mut buf = Vec::with_capacity(SIZE);
+    let mut rt = Runtime::new().expect("Unable to create a runtime");
 
     for m in measurements.0 {
-        let mut measurement = Measurement::new("energiatili");
-
         let ts = m.timestamp.timestamp_nanos();
-        measurement.set_timestamp(ts);
+        let timestamp = influxdb::Timestamp::NANOSECONDS(ts as usize);
+        let mut measurement = influxdb::Query::write_query(timestamp, "energiatili");
 
-        measurement.add_field("consumption", Value::Float(m.consumption));
-        measurement.add_field("quality", Value::Integer(i64::from(m.quality)));
+        measurement = measurement.add_field("consumption", m.consumption);
+        measurement = measurement.add_field("quality", i64::from(m.quality));
 
         if let Some(price) = m.price.energy {
-            measurement.add_field("energy_price", Value::Float(price));
+            measurement = measurement.add_field("energy_price", price);
         }
 
         if let Some(price) = m.price.transfer {
-            measurement.add_field("transfer_price", Value::Float(price));
+            measurement = measurement.add_field("transfer_price", price);
         }
 
         if let (Some(e), Some(t)) = (m.price.energy, m.price.transfer) {
-            measurement.add_field("price", Value::Float(e + t));
+            measurement = measurement.add_field("price", e + t);
         }
 
         if !m.temperature.is_nan() {
-            measurement.add_field("temperature", Value::Float(m.temperature));
+            measurement = measurement.add_field("temperature", m.temperature);
         }
 
         if m.resolution == Resolution::Hour {
-            match m.tariff {
+            measurement = match m.tariff {
                 Tariff::Day => measurement.add_tag("tariff", "day"),
                 Tariff::Night => measurement.add_tag("tariff", "night"),
-            }
+            };
         }
 
-        match m.resolution {
+        measurement = match m.resolution {
             Resolution::Hour => measurement.add_tag("resolution", "hour"),
             Resolution::Day => measurement.add_tag("resolution", "day"),
             Resolution::Month => measurement.add_tag("resolution", "month"),
             Resolution::Year => measurement.add_tag("resolution", "year"),
-        }
+        };
 
-        buf.push(measurement);
-
-        if buf.len() == SIZE {
-            influxdb.write_many(&buf, None).expect("influent write_many");
-            buf.clear();
-        }
+        rt.block_on(client.query(&measurement)).expect("influxdb write");
     }
-
-    influxdb.write_many(&buf, None).expect("influent write_many");
 }
