17dca5ce496b10c9c8f5e31f9d7f0b02c89be4f7
Ossi Herrala <oherrala@iki.fi>
2020-05-22 14:32:17 +0300

    Update reqwest 0.10 and convert to async

--- energiatili-import/src/main.rs
+++ energiatili-import/src/main.rs
@@ -9,7 +9,8 @@ const FIRST_URL: &str = "/Extranet/Extranet";
 const LOGIN_URL: &str = "/Extranet/Extranet/LogIn";
 const REPORT_URL: &str = "/Reporting/CustomerConsumption/UserConsumptionReport";
 
-fn main() -> io::Result<()> {
+#[tokio::main]
+async fn main() {
     env_logger::init();
 
     let config = match energiatili_config::Config::read() {
@@ -22,9 +23,19 @@ fn main() -> io::Result<()> {
     let username = &config.energiatili.username;
     let password = &config.energiatili.password;
 
-    let mut client = Client::new().expect("new client");
-    client.login(username, password).expect("Client login");
-    let report = client.consumption_report().expect("consumption_report");
+    match run(username, password).await {
+        Ok(()) => (),
+        Err(err) => {
+            eprintln!("ERROR: {}", err);
+            std::process::exit(1);
+        }
+    }
+}
+
+async fn run(username: &str, password: &str) -> Result<()> {
+    let mut client = Client::new().await?;
+    client.login(username, password).await?;
+    let report = client.consumption_report().await?;
     debug!("Consumption report HTML:\n{}\n", report);
 
     let cursor = io::Cursor::new(report);
@@ -38,60 +49,76 @@ struct Client {
     client: reqwest::Client,
 }
 
+type Result<T> = std::result::Result<T, Error>;
+
 #[derive(Debug)]
 enum Error {
+    JSON(serde_json::Error),
     Reqwest(reqwest::Error),
 }
 
-type Result<T> = std::result::Result<T, Error>;
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Error::JSON(err) => err.source(),
+            Error::Reqwest(err) => err.source(),
+        }
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::JSON(err) => writeln!(f, "JSON error: {}", err),
+            Error::Reqwest(err) => writeln!(f, "HTTP error: {}", err),
+        }
+    }
+}
 
 impl Client {
-    fn new() -> Result<Client> {
+    async fn new() -> Result<Client> {
         trace!("Client::new()");
         let client = reqwest::ClientBuilder::new()
-            .redirect(reqwest::RedirectPolicy::none())
+            .redirect(reqwest::redirect::Policy::none())
             .cookie_store(true)
             .build()?;
 
-        let req = client.get(&format!("{}{}", BASE_URL, FIRST_URL))
-            .build()?;
+        let url = format!("{}{}", BASE_URL, FIRST_URL);
+        let req = client.get(&url).build()?;
         debug!("GET Request: {:?}", req);
 
-        let resp = client.execute(req)?;
+        let resp = client.execute(req).await?;
         debug!("GET Response: {:?}", resp);
 
         Ok(Self { client })
     }
 
-    fn login(&mut self, username: &str, password: &str) -> Result<()> {
+    async fn login(&mut self, username: &str, password: &str) -> Result<()> {
         trace!("Client::login({:?})", self);
 
+        let url = format!("{}{}", BASE_URL, LOGIN_URL);
         let params = [("username", username), ("password", password)];
-        let req = self.client.post(&format!("{}{}", BASE_URL, LOGIN_URL))
-            .form(&params)
-            .build()?;
+        let req = self.client.post(&url).form(&params).build()?;
         debug!("POST Request: {:?}", req);
 
-        let resp = self.client.execute(req)?;
+        let resp = self.client.execute(req).await?;
         debug!("POST Response: {:?}", resp);
 
         Ok(())
     }
 
-    fn consumption_report(&self) -> Result<String> {
+    async fn consumption_report(&self) -> Result<String> {
         trace!("Client::consumption_report({:?})", self);
         let client = &self.client;
 
-        let req = client.get(&format!("{}{}", BASE_URL, REPORT_URL))
-            .build().expect("Build consumption report request");
+        let url = format!("{}{}", BASE_URL, REPORT_URL);
+        let req = client.get(&url).build()?;
         debug!("GET Request: {:?}", req);
 
-        let mut resp = client.execute(req)?;
+        let resp = client.execute(req).await?;
         debug!("GET Response: {:?}", resp);
 
-        let mut buf: Vec<u8> = Vec::new();
-        resp.copy_to(&mut buf)?;
-        let res = String::from_utf8(buf).expect("from_utf8");
+        let res = resp.text().await?;
         Ok(res)
     }
 }
@@ -99,5 +126,11 @@ impl Client {
 impl From<reqwest::Error> for Error {
     fn from(err: reqwest::Error) -> Self {
         Error::Reqwest(err)
+    }
+}
+
+impl From<serde_json::Error> for Error {
+    fn from(err: serde_json::Error) -> Self {
+        Error::JSON(err)
     }
 }
